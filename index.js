d1384f09dfa9c875707aa56282b8fd0c79142267
Mastersam07 <abadasamuelosp@gmail.com>
2022-11-19 15:34:39 +0100

    :card_file_box: chore: update urls

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const functions = require("firebase-functions");
 const app = require("express")(); // Import and initialize express
 const cors = require("cors");
 
-app.use( cors( {origin: true} ) );
+app.use(cors({ origin: true }));
 
-const {speakers} = require("./handlers/speakers.handler");
-const {checkIn} = require("./handlers/checkin.handler");
-const {categories} = require("./handlers/categories.handler");
-const {halls} = require("./handlers/halls.handler");
-const {organizers} = require("./handlers/organizers.handler");
-const {sponsors} = require("./handlers/sponsors.handler");
+const { speakers } = require("./handlers/speakers.handler");
+const { checkIn } = require("./handlers/checkin.handler");
+const { categories } = require("./handlers/categories.handler");
+const { halls } = require("./handlers/halls.handler");
+const { organizers } = require("./handlers/organizers.handler");
+const { sponsors } = require("./handlers/sponsors.handler");
+const { postsponsor } = require("./handlers/post.sponsors.handler");
+const { postspeakers } = require("./handlers/post.speakers.handler");
+const { postsessions } = require("./handlers/post.session.handler");
+const { postcategory } = require("./handlers/post.category.handler");
+const { postorganizers } = require("./handlers/post.organizers.handler");
 
 
 // Main Routes
@@ -18,6 +23,11 @@ app.get("/category", categories);
 app.get("/venue", halls);
 app.get("/team", organizers);
 app.get("/sponsors", sponsors);
+app.post("/postsponsors", postsponsor);
+app.post("/postspeakers", postspeakers);
+app.post("/postsessions", postsessions);
+app.post("/postcategory", postcategory);
+app.post("/postorganizers", postorganizers);
 app.post("/checkin", checkIn);
 
 exports.api = functions.https.onRequest(app);
