0dcee3f746c7aadf805d9ed76bb48e18004a1b1a
Mastersam07 <abadasamuelosp@gmail.com>
2022-11-19 15:33:15 +0100

    :card_file_box: chore: upload speakers data

---
+++ handlers/post.speakers.handler.js
@@ -0,0 +1,27 @@
+const data = require("./speakers.json");
+
+const functions = require("firebase-functions");
+const { db } = require("../util/admin");
+
+exports.postspeakers = async (req, res) => {
+    try {
+        data.forEach(function (doc) {
+            db.collection("Speaker").doc(doc["Email"]).set({
+                avatar: doc["Profile Picture"],
+                bio: doc["Bio"],
+                name: doc["FirstName"] + " " + doc["LastName"],
+                email: doc["Email"],
+                organization: doc["TagLine"],
+                role: doc["TagLine"],
+                github: doc["GitHub Handle"],
+                twitter: doc["Twitter"],
+                linkedIn: doc["LinkedIn"],
+            });
+
+        });
+        return res.status(200).send({ "message": "Speakers uploaded successfully!" });
+    } catch (error) {
+        console.log(error);
+        return res.status(500);
+    }
+};
