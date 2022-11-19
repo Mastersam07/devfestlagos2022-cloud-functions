593708dfb23abc6458285d676be01eabaebb7ee3
Mastersam07 <abadasamuelosp@gmail.com>
2022-11-19 15:32:26 +0100

    :card_file_box: chore: upload sponsors data

---
+++ handlers/post.session.handler.js
@@ -0,0 +1,40 @@
+const data = require("./session.json");
+const speakers = require("./speakers.json");
+
+const functions = require("firebase-functions");
+const { db } = require("../util/admin");
+
+exports.postsessions = async (req, res) => {
+    try {
+        data.forEach(function (doc) {
+            speakers.forEach(function (speaker) {
+                if (doc["Owner Email"] == speaker["Email"]) {
+                    db.collection("Session").doc(doc["Session Id"].toString()).set(
+                        {
+                            sessionId: doc["Session Id"].toString(),
+                            title: doc["Title"],
+                            description: doc["Description"],
+                            owner: doc["Owner"],
+                            ownerEmail: doc["Owner Email"],
+                            sessionFormat: doc["Session format"],
+                            category: doc["Track"],
+                            level: doc["Level"],
+                            hall: doc["Room"],
+                            scheduledAt: doc["Scheduled At"],
+                            scheduledDuration: doc["Scheduled Duration"],
+                            speakerImage: speaker["Profile Picture"],
+                            tagLine: speaker["TagLine"],
+                        });
+                } else {
+                    console.log("Sope Otilor");
+                }
+
+            });
+
+        });
+        return res.status(200).send({ "message": "Sessions uploaded successfully!" });
+    } catch (error) {
+        console.log(error);
+        return res.status(500);
+    }
+};
